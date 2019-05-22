308e51fc1e21f1cbf3357b9b9275084f2e80ec05
bren <bren@153.io>
2019-05-22 07:48:43 +0100

    rendering vertices, adjacent vertices

--- index.jsx
+++ index.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
-import styled from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 import { Record, Set } from 'immutable'
 
 class TriangleGridEdge extends Record({ u: 0, v: 0, s: 'W' }) {}
 
-class TriangleGridVertex extends Record({ u: 0, v: 0, s: 'L' }) {
+const ROOT3_2 = Math.sin(Math.PI / 3)
+
+class TriangleGridVertex extends Record({ u: 0, v: 0 }) {
+	get x() {
+		return this.u + 0.5 * this.v
+	}
+
+	get y() {
+		return ROOT3_2 * this.v
+	}
+
 	get protrudes() {
 		return Set.of(
 			new TriangleGridEdge({ u: this.u, v: this.v, s: 'W' }),
@@ -16,8 +26,44 @@ class TriangleGridVertex extends Record({ u: 0, v: 0, s: 'L' }) {
 			new TriangleGridEdge({ u: this.u - 1, v: this.v, s: 'E' }),
 		)
 	}
+
+	get adjacent() {
+		return Set.of(
+			new TriangleGridVertex({ u: this.u, v: this.v + 1 }),
+			new TriangleGridVertex({ u: this.u + 1, v: this.v }),
+			new TriangleGridVertex({ u: this.u + 1, v: this.v - 1 }),
+			new TriangleGridVertex({ u: this.u, v: this.v - 1 }),
+			new TriangleGridVertex({ u: this.u - 1, v: this.v }),
+			new TriangleGridVertex({ u: this.u - 1, v: this.v + 1 }),
+		)
+	}
 }
 
-console.log(new TriangleGridVertex().protrudes.toJS())
+const Vertex = styled.div`
+	width: ${({ theme }) => theme.scale / 10}px;
+	height: ${({ theme }) => theme.scale / 10}px;
+	background: ${({ theme }) => theme.colour || 'black'};
+	border-radius: 100%;
+	position: absolute;
+	top: ${({ theme, vertex }) => theme.scale * vertex.y}px;
+	left: ${({ theme, vertex }) => theme.scale * vertex.x}px;
+`
 
-render(<h1>it's hot</h1>, document.querySelector('main'))
+const v = new TriangleGridVertex({ u: 1, v: 2 })
+
+render(
+	<ThemeProvider theme={{ scale: 100 }}>
+		<>
+			<Vertex vertex={v} />
+
+			{v.adjacent.valueSeq().map(a => (
+				<Vertex
+					key={`${a.u},${a.v}`}
+					vertex={a}
+					theme={{ scale: 100, colour: 'red' }}
+				/>
+			))}
+		</>
+	</ThemeProvider>,
+	document.querySelector('main'),
+)
