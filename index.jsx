6e6d0ea18cec383259b5768e04145dc2642a82f6
bren <bren@153.io>
2019-05-22 08:07:44 +0100

    working edge renderer
    i don't like the new coordinates for edges but whatever

--- index.jsx
+++ index.jsx
@@ -3,7 +3,23 @@ import { render } from 'react-dom'
 import styled, { ThemeProvider } from 'styled-components'
 import { Record, Set } from 'immutable'
 
-class TriangleGridEdge extends Record({ u: 0, v: 0, s: 'W' }) {}
+class TriangleGridEdge extends Record({ u: 0, v: 0, s: 'W' }) {
+	get x() {
+		return this.u + 0.5 * this.v
+	}
+
+	get y() {
+		return ROOT3_2 * this.v
+	}
+
+	get angle() {
+		return {
+			E: 0,
+			S: 60,
+			N: -60,
+		}[this.s]
+	}
+}
 
 const ROOT3_2 = Math.sin(Math.PI / 3)
 
@@ -18,12 +34,12 @@ class TriangleGridVertex extends Record({ u: 0, v: 0 }) {
 
 	get protrudes() {
 		return Set.of(
-			new TriangleGridEdge({ u: this.u, v: this.v, s: 'W' }),
+			new TriangleGridEdge({ u: this.u, v: this.v, s: 'N' }),
+			new TriangleGridEdge({ u: this.u, v: this.v, s: 'E' }),
 			new TriangleGridEdge({ u: this.u, v: this.v, s: 'S' }),
-			new TriangleGridEdge({ u: this.u, v: this.v - 1, s: 'E' }),
-			new TriangleGridEdge({ u: this.u, v: this.v - 1, s: 'W' }),
-			new TriangleGridEdge({ u: this.u - 1, v: this.v, s: 'S' }),
+			new TriangleGridEdge({ u: this.u, v: this.v - 1, s: 'S' }),
 			new TriangleGridEdge({ u: this.u - 1, v: this.v, s: 'E' }),
+			new TriangleGridEdge({ u: this.u - 1, v: this.v + 1, s: 'N' }),
 		)
 	}
 
@@ -45,8 +61,24 @@ const Vertex = styled.div`
 	background: ${({ theme }) => theme.colour || 'black'};
 	border-radius: 100%;
 	position: absolute;
-	top: ${({ theme, vertex }) => theme.scale * vertex.y}px;
-	left: ${({ theme, vertex }) => theme.scale * vertex.x}px;
+	top: ${({ theme, vertex }) => theme.scale * vertex.y - theme.scale / 20}px;
+	left: ${({ theme, vertex }) => theme.scale * vertex.x - theme.scale / 20}px;
+
+	&::after {
+		content: "${({ vertex }) => `${vertex.u},${vertex.v}`}";
+		margin-left: 1em;
+	}
+`
+
+const Edge = styled.div`
+	width: ${({ theme }) => theme.scale}px;
+	height: ${({ theme }) => theme.scale / 20}px;
+	background: ${({ theme }) => theme.colour || 'black'};
+	position: absolute;
+	top: ${({ theme, edge }) => theme.scale * edge.y - theme.scale / 40}px;
+	left: ${({ theme, edge }) => theme.scale * edge.x}px;
+	transform-origin: left center;
+	transform: rotate(${({ edge }) => edge.angle}deg);
 `
 
 const v = new TriangleGridVertex({ u: 1, v: 2 })
@@ -61,6 +93,14 @@ render(
 					key={`${a.u},${a.v}`}
 					vertex={a}
 					theme={{ scale: 100, colour: 'red' }}
+				/>
+			))}
+
+			{v.protrudes.valueSeq().map(e => (
+				<Edge
+					key={`${e.u},${e.v},${e.s}`}
+					edge={e}
+					data-edge={JSON.stringify(e)}
 				/>
 			))}
 		</>
