9282aa27dcabe1d99f544c9be837dddf01d16941
bren <bren@153.io>
2019-05-22 09:57:43 +0100

    adding nodes to a  network

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { render } from 'react-dom'
 import styled, { ThemeProvider } from 'styled-components'
-import { Record, Set } from 'immutable'
+import { Record, Set, List } from 'immutable'
 
 class TriangleGridEdge extends Record({ u: 0, v: 0, s: 'W' }) {
 	get x() {
@@ -18,6 +18,26 @@ class TriangleGridEdge extends Record({ u: 0, v: 0, s: 'W' }) {
 			E: 60,
 			W: -60,
 		}[this.s]
+	}
+
+	get endpoints() {
+		switch (this.s) {
+			case 'W':
+				return Set.of(
+					new TriangleGridVertex({ u: this.u, v: this.v + 1 }),
+					new TriangleGridVertex({ u: this.u, v: this.v }),
+				)
+			case 'E':
+				return Set.of(
+					new TriangleGridVertex({ u: this.u, v: this.v + 1 }),
+					new TriangleGridVertex({ u: this.u + 1, v: this.v }),
+				)
+			case 'S':
+				return Set.of(
+					new TriangleGridVertex({ u: this.u + 1, v: this.v }),
+					new TriangleGridVertex({ u: this.u, v: this.v }),
+				)
+		}
 	}
 }
 
@@ -66,6 +86,8 @@ const Vertex = styled.div`
 	left: calc(50vw + ${({ theme, vertex }) =>
 		theme.scale * vertex.x - theme.scale / 20}px);
 
+	z-index: 1;
+
 	main.debug &::after {
 		content: "${({ vertex }) => `${vertex.u},${vertex.v}`}";
 		margin-left: 1em;
@@ -75,7 +97,7 @@ const Vertex = styled.div`
 const Edge = styled.div`
 	width: ${({ theme }) => theme.scale}px;
 	height: ${({ theme }) => theme.scale / 20}px;
-	background: ${({ theme }) => theme.colour || 'black'};
+	background: ${({ theme, colour }) => colour || theme.colour || 'black'};
 	position: absolute;
 	top: calc(
 		50vh + ${({ theme, edge }) => theme.scale * edge.y - theme.scale / 40}px
@@ -92,27 +114,48 @@ const Edge = styled.div`
 
 const v = new TriangleGridVertex()
 
-render(
-	<ThemeProvider theme={{ scale: 100 }}>
-		<>
-			<Vertex vertex={v} />
+const useImmutableState = initial => {
+	const [state, setState] = useState(initial)
+	return [state, fn => setState(current => current.withMutations(fn))]
+}
 
-			{v.adjacent.valueSeq().map(a => (
+const Network = () => {
+	const [vertices, updateVertices] = useImmutableState(
+		Set.of(new TriangleGridVertex({ u: 0, v: 0 })),
+	)
+
+	const possibleEdges = vertices.flatMap(v => v.protrudes).toSet()
+
+	const addVertexForEdge = edge => () => {
+		updateVertices(vs => edge.endpoints.forEach(v => vs.add(v)))
+	}
+
+	return (
+		<>
+			{vertices.valueSeq().map(v => (
 				<Vertex
-					key={`${a.u},${a.v}`}
-					vertex={a}
+					key={`${v.u},${v.v}`}
+					vertex={v}
 					theme={{ scale: 100, colour: 'red' }}
 				/>
 			))}
 
-			{v.protrudes.valueSeq().map(e => (
+			{possibleEdges.valueSeq().map(e => (
 				<Edge
 					key={`${e.u},${e.v},${e.s}`}
 					edge={e}
 					data-edge={JSON.stringify(e)}
+					colour='grey'
+					onClick={addVertexForEdge(e)}
 				/>
 			))}
 		</>
+	)
+}
+
+render(
+	<ThemeProvider theme={{ scale: 100 }}>
+		<Network />
 	</ThemeProvider>,
 	document.querySelector('main'),
 )
